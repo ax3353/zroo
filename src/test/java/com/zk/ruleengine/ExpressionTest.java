d0afb1e1d0e28b2f2d43b2e2305c48931d928fe9
kun.zhu <247213579@qq.com>
2024-12-23 09:40:13 +0800

    test

--- src/test/java/com/zk/ruleengine/ExpressionTest.java
+++ src/test/java/com/zk/ruleengine/ExpressionTest.java
@@ -17,6 +17,7 @@ public class ExpressionTest {
         context.put("phone", "18576481122");
         context.put("sex", "男");
         context.put("id", "12");
+        context.put("idx", 12);
         context.put("hidden_danger_code", "13");
         context.put("danger_code", 0);
         context.put("danger_status", 100);
@@ -29,6 +30,7 @@ public class ExpressionTest {
         context.put("flow_meter_code", "123");
         context.put("report_time", "2024-11-20");
         context.put("nowDate", "2024-11-20");
+        context.put("ecode", "130");
 
         System.out.println("------------------------------0");
 //        String exp = "[\"if\",[\"&&\",[\"strEq\",[\"@value\",\"deviceName\"],[\"strInput\",\"河庄门店\"]],[\"&&\",[\">=\",[\"*\",[\"@value\",\"pressure\"],[\"numberInput\",5]],[\"numberInput\",30]],[\"<=\",[\"*\",[\"@value\",\"pressure\"],[\"numberInput\",5]],[\"numberInput\",50]]]],[\"print\",\"执行操作A\"],[\"if\",[\"==\",[\"dayBetween\",[\"@value\",\"alarmTime\"],[\"@value\",\"createTime\"]]],[\"print\",\"执行操作B\"],[\"print\",\"没有满足任何条件时执行操作C\"]]]";
@@ -100,5 +102,10 @@ public class ExpressionTest {
         System.out.println(exp9);
         Object eval9 = engine.execute(context, exp9);
         System.out.println(eval9);
+
+        String exp10 = "[\"if\",[\"&&\",[\"==\",[\"@value\",\"idx\"],[\"numberInput\",12]],[\"||\",[\"contains\",[\"@value\",\"ecode\"],[\"numberInput\",13]],[\"notContains\",[\"@value\",\"station_code\"],[\"numberInput\",14]]]],[\"strEq\",[\"@value\",\"hidden_danger_code\"],[\"numberInput\",13]],[\"notContains\",[\"@value\",\"station_name\"],[\"numberInput\",123]]]";
+        System.out.println(exp10);
+        Object eval10 = engine.execute(context, exp10);
+        System.out.println(eval10);
     }
 }
